6344c9b1f16467fd2fbea070a87aade26a0aae13
daisy <daisy@daisyMAcBook>
2020-05-24 16:26:39 +0800

    First Test3

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -3,5 +3,7 @@ import sun.lwawt.macosx.CSystemTray;
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
+        System.out.println("Hello world!!!");
+        System.out.println("Hello world!!!");
     }
 }
